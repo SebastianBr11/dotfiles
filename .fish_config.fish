e4bced7443be33d21d84fcd7a33a919ad373f450
Sebastian Breit <sebastian.breitan+github@gmail.com>
2026-03-07 17:33:08 +0100

    Add brew completions for fish

--- .fish_config.fish
+++ .fish_config.fish
@@ -2,6 +2,13 @@ mise activate fish | source
 starship init fish | source
 brew shellenv | source
 
+if test -d (brew --prefix)/share/fish/completions
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+if test -d (brew --prefix)/share/fish/vendor_completions.d
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
+
 # Put brew at the end of the path
 fish_add_path -a -P -g --move "/home/linuxbrew/.linuxbrew/bin"
 fish_add_path -a -P -g --move "/home/linuxbrew/.linuxbrew/sbin"
