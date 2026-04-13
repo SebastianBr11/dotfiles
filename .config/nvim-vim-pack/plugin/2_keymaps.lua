8f189678c4c55e707d8b3166ac89bac7d49468e6
Sebastian Breit <s.breit@snapaddy.com>
2026-04-13 10:31:55 +0200

    (nvim): add keymaps

--- .config/nvim-vim-pack/plugin/2_keymaps.lua
+++ .config/nvim-vim-pack/plugin/2_keymaps.lua
@@ -1,1 +1,30 @@
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+local set = vim.keymap.set
+
+for i = 1, 9 do
+	set({ "i", "n" }, "<C-" .. i .. ">", i .. "gt", { unique })
+end
+
+set({ "i", "n" }, "<C-0>", ":tablast<CR>")
+
+-- Thanks TJ
+set("n", "j", function(...)
+	local count = vim.v.count
+
+	if count == 0 then
+		return "gj"
+	else
+		return "j"
+	end
+end, { expr = true })
+
+set("n", "k", function(...)
+	local count = vim.v.count
+
+	if count == 0 then
+		return "gk"
+	else
+		return "k"
+	end
+end, { expr = true })
