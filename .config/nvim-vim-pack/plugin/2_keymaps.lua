7806aafc9086da9f47dc5de28d9fbb5cc319e745
Sebastian Breit <sebastian.breitan+github@gmail.com>
2026-04-16 18:33:35 +0200

    (nvim): add custom keymaps for incremental selection

--- .config/nvim-vim-pack/plugin/2_keymaps.lua
+++ .config/nvim-vim-pack/plugin/2_keymaps.lua
@@ -1,13 +1,12 @@
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
 local set = vim.keymap.set
+set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 for i = 1, 9 do
 	set({ "i", "n" }, "<C-" .. i .. ">", i .. "gt", { unique })
@@ -35,3 +34,20 @@ set("n", "k", function(...)
 		return "k"
 	end
 end, { expr = true })
+
+-- incremental selection treesitter/lsp
+vim.keymap.set({ "n", "x", "o" }, "<A-o>", function()
+	if vim.treesitter.get_parser(nil, nil, { error = false }) then
+		require("vim.treesitter._select").select_parent(vim.v.count1)
+	else
+		vim.lsp.buf.selection_range(vim.v.count1)
+	end
+end, { desc = "Select parent treesitter node or outer incremental lsp selections" })
+
+vim.keymap.set({ "n", "x", "o" }, "<A-i>", function()
+	if vim.treesitter.get_parser(nil, nil, { error = false }) then
+		require("vim.treesitter._select").select_child(vim.v.count1)
+	else
+		vim.lsp.buf.selection_range(-vim.v.count1)
+	end
+end, { desc = "Select child treesitter node or inner incremental lsp selections" })
