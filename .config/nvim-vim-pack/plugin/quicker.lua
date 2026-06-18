5096d68465327a110e478846790e0c04e6d15fe3
Sebastian Breit <s.breit@snapaddy.com>
2026-06-18 09:18:11 +0200

    fix(nvim): remove duplicate quickfix list keymap

--- .config/nvim-vim-pack/plugin/quicker.lua
+++ .config/nvim-vim-pack/plugin/quicker.lua
@@ -3,11 +3,6 @@ vim.pack.add({
 })
 
 require("quicker").setup()
-vim.keymap.set("n", "<leader>q", function()
-  require("quicker").toggle()
-end, {
-  desc = "Toggle quickfix",
-})
 vim.keymap.set("n", "<leader>l", function()
   require("quicker").toggle({ loclist = true })
 end, {
