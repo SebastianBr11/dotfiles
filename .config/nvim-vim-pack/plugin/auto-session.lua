3bf6ca4e0e0b7cf9cedde6d3c117de49399baa97
Sebastian Breit <41546861+SebastianBr11@users.noreply.github.com>
2026-07-24 17:16:38 +0200

    (nvim): auto restore last session if nvim is opened from home dir

--- .config/nvim-vim-pack/plugin/auto-session.lua
+++ .config/nvim-vim-pack/plugin/auto-session.lua
@@ -1,5 +1,7 @@
 vim.pack.add({ "https://github.com/rmagatti/auto-session" })
-require("auto-session").setup({})
+require("auto-session").setup({
+  auto_restore_last_session = vim.loop.cwd() == vim.fn.expand("~"),
+})
 
 vim.keymap.set("n", "<leader>wr", "<cmd>AutoSession search<CR>", { desc = "Session search" })
 vim.keymap.set("n", "<leader>ws", "<cmd>AutoSession save<CR>", { desc = "[S]ave session" })
