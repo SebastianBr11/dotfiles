21f9d86ff27e327c790bde0e15fbed7f890321e7
Sebastian Breit <41546861+SebastianBr11@users.noreply.github.com>
2026-07-25 15:02:10 +0200

    (nvim): set kanagawa gutter background to transparent

--- .config/nvim-vim-pack/plugin/6_colorscheme.lua
+++ .config/nvim-vim-pack/plugin/6_colorscheme.lua
@@ -2,5 +2,16 @@ vim.pack.add({
   "https://github.com/rebelot/kanagawa.nvim",
 })
 
-require("kanagawa").setup({ transparent = true })
+require("kanagawa").setup({
+  transparent = true,
+  colors = {
+    theme = {
+      all = {
+        ui = {
+          bg_gutter = "none",
+        },
+      },
+    },
+  },
+})
 vim.cmd.colorscheme("kanagawa")
