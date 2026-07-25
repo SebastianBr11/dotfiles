f4dadf517175e228b29cc49834744de18136c312
Sebastian Breit <41546861+SebastianBr11@users.noreply.github.com>
2026-07-25 18:48:22 +0200

    (shell-common): add alias to edit dotfiles using nvim

--- .shell-common.sh
+++ .shell-common.sh
@@ -6,7 +6,6 @@ export MANPAGER='nvim +Man!' # Use neovim as manpager
 export ANDROID_HOME="$HOME/Android/Sdk"
 export ANDROID_SDK_ROOT="$HOME/Android/Sdk"
 
-
 # Aliases
 
 alias nvimc="cd ~/.config/nvim && nvim"
@@ -16,3 +15,4 @@ alias nvim-pack="NVIM_APPNAME=nvim-vim-pack nvim"
 alias nvimc-pack="cd ~/.config/nvim-vim-pack && nvim-pack"
 alias nv="nvim-pack"
 alias nvc="nvimc-pack"
+alias nvd="cd ~/.dotfiles && nv"
