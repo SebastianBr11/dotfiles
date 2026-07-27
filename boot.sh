4cbe5f82ecb05dbce0ed78939c7b432db77e96d1
Sebastian Breit <41546861+SebastianBr11@users.noreply.github.com>
2026-07-27 09:24:05 +0200

    (boot): create empty .config/mise/config.toml file

--- boot.sh
+++ boot.sh
@@ -6,3 +6,8 @@ if ! command -v mise >/dev/null 2>&1; then
 fi
 
 mise bootstrap
+
+# Create an empty config file which will be used when running
+# e.g. mise use -g to test stuff out, which should not be accidentally
+# committed in the dotfiles.
+touch ~/.config/mise/config.toml
